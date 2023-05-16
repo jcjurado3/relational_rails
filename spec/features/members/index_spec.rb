49387fd257ced9dfe4fe2796bd166f2d09cb20a2
Jimmy Jurado <jurado140@gmail.com>
2023-05-16 16:46:21 -0400

    Completes User Story 15.

--- spec/features/members/index_spec.rb
+++ spec/features/members/index_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe "member index page", type: :feature do
     @gym_2 = Gym.create!(name:                     "HSD Fit",
                         location:          "Boca Raton, FL",
                         franchise_owner: "Carmine Scandura")
+
     @member_1 = @gym.members.create!(name:        "John House",
                                     age:                   52,
                                     monthly_dues:         199,
@@ -19,6 +20,7 @@ RSpec.describe "member index page", type: :feature do
                                     monthly_dues:         139,
                                     program_type:    "3 days",
                                     active_member:      true)
+
     @member_3 = @gym_2.members.create!(name:"Cristina Waterson",
                                       age:                   62,
                                       monthly_dues:         199,
@@ -29,6 +31,12 @@ RSpec.describe "member index page", type: :feature do
                                       monthly_dues:         139,
                                       program_type:    "3 days",
                                       active_member:      true)
+    @member_5 = @gym_2.members.create!(name:   "Kelly Carter",
+                                      age:                   39,
+                                      monthly_dues:         139,
+                                      program_type:    "3 days",
+                                      active_member:      false)
+                                      
   end
 
   it "can display each member in the system with their attributes" do
@@ -51,7 +59,7 @@ RSpec.describe "member index page", type: :feature do
   it "Has link at the top of the page for Member Index" do
     visit "/members"
 
-    expect(page.has_link?).to be(true)
+    expect(page).to have_link("All Members")
 
     click_link("All Members")
 
@@ -64,11 +72,21 @@ RSpec.describe "member index page", type: :feature do
   it "Has link at the top of the page for Gym Index" do
     visit "/members"
 
-    expect(page.has_link?).to be(true)
+    expect(page).to have_link("All Gyms")
 
     click_link("All Gyms")
 
     expect(page).to have_content("#{@gym.name}")
     expect(page).to have_content("#{@gym_2.name}")
+  end
+
+  it "Member Index only shows true Records" do
+    visit "/members"
+
+    expect(page).to have_content("Member Name: #{@member_1.name}")
+    expect(page).to have_content("Member Name: #{@member_2.name}")
+    expect(page).to have_content("Member Name: #{@member_3.name}")
+    expect(page).to_not have_content("Member Name: #{@member_5.name}")
+
   end
 end
